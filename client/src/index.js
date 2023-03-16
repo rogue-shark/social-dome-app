5174d77e1018f30ff40bdb05833abf66b4991fca
RogueShark <shantanuss789@gmail.com>
2023-03-16 22:53:26 +0530

    Added: Redux / Toolkit logic
    https://redux-toolkit.js.org/tutorials/quick-start
    
    - Setting up Redux store configurations to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; 
+import authReducer from './state';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+//for redux persist -- to save user info in local state i.e cached
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: 'root', storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        //ignore warnings
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
