b72b091c5f80b26eea4015884fd6b99148a1b514
RogueShark <shantanuss789@gmail.com>
2023-03-20 19:54:44 +0530

    Added: Friend List & Advert Widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import Navbar from "scenes/navbar"
 import UserDetailsWidget from "scenes/widgets/UserDetailsWidget"
 import MyPostWidget from 'scenes/widgets/MyPostWidget'
 import PostsWidget from "scenes/widgets/PostsWidget"
+import AdvertWidget from "scenes/widgets/AdvertWidget"
+import FriendListWidget from "scenes/widgets/FriendListWidget"
 
 const HomePage = () => {
   
@@ -35,7 +37,10 @@ const HomePage = () => {
 
         {isNonMobileScreens && (
           <Box flexBasis='26%'>
-            
+            <AdvertWidget />
+            <Box m='2rem 0'>
+              <FriendListWidget userId={_id} />
+            </Box>
           </ Box>
         )}
         
